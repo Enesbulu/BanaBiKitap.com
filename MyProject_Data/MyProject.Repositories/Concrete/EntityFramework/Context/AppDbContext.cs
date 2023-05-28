d0fec49b80bbf4ff64d310311ab0bf766070410d
Enes BULUT <bulut0258@gmail.com>
2023-05-28 23:14:33 +0300

    Data Katmanı

---
+++ MyProject_Data/MyProject.Repositories/Concrete/EntityFramework/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyProject_Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Repositories.Concrete.EntityFramework.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=.;initial catalog=DbConn;persist security info=True;user id=sa;password=123");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //devam edilecek
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
